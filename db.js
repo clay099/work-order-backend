7703b40a598bedc0edfbf214254d85b3d7d3a13e
Clayton Whittaker <clayton.whittaker@gmail.com>
2021-04-28 22:46:38 -0700

    ssl requled for porduction

--- db.js
+++ db.js
@@ -4,7 +4,9 @@ const { Client } = require("pg");
 const { DB_URI } = require("./config");
 
 const client = new Client({
-	connectionString: DB_URI,
+	connectionString: `${DB_URI}${
+		process.env.NODE_ENV === "production" ? "sslmode=require" : ""
+	}`,
 });
 
 console.log({ client });
